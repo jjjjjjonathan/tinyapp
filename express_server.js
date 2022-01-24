7e5484060c8302cc8a6fe3552546837679f0cb74
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-24 18:18:15 -0500

    Added template and seond route to /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -38,3 +38,8 @@ app.get("/set", (req, res) => {
    const templateVars = { urls: urlDatabase };
    res.render("urls_index", templateVars);
  });
+
+ app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
