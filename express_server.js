3137aeafd230dd6b0f6df39833e0038f7f37b3b0
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-26 14:02:52 -0500

    removed mentions of username for cookies, sticking to only using user_id

--- express_server.js
+++ express_server.js
@@ -56,17 +56,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies['username'], user: users[req.cookies['user_id']] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies['username'], user: users[req.cookies['user_id']] };
+  const templateVars = { user: users[req.cookies['user_id']] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username'], user: users[req.cookies['user_id']] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies['user_id']] };
   res.render("urls_show", templateVars);
 });
 
@@ -114,7 +114,7 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies['username'], user: users[req.cookies['user_id']] };
+  const templateVars = { user: users[req.cookies['user_id']] };
   res.render("register", templateVars);
 });
 
