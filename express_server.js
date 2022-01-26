79b9c2f61c8a4f8e1724382538b5a10ef20423d8
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-25 20:44:07 -0500

    Changed header to show username and logout button if cookie is present

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -9,6 +11,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const templateVars = {
+  urls: urlDatabase,
+  username: undefined
 };
 
 const generateRandomString = () => {
@@ -41,12 +48,14 @@ app.get("/fetch", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  if (req.cookies['username'] !== undefined) {
+    templateVars['username'] = req.cookies['username'];
+  }
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
